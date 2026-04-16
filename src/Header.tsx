9913f40cfedb2fe24ef74b7a257369b58acc7232
Guvnoh <asogwachukwuka@gmail.com>
2026-04-16 17:53:05 +0100

    warming up...

---
+++ src/Header.tsx
@@ -0,0 +1,8 @@
+import "./header.css";
+export default function Header() {
+  return (
+    <header className="header bg-gray-800 text-white p-4">
+      <h1 className="text-2xl font-bold">Nori's Stitches</h1>
+    </header>
+  );
+}
