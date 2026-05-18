8be78196ba0145524ce89683e18aeb27a2d58e8a
Guvnoh <asogwachukwuka@gmail.com>
2026-05-18 16:52:40 +0100

    fix connection to server issues

--- Noris_Stitches/src/tools/db_interface.ts
+++ Noris_Stitches/src/tools/db_interface.ts
@@ -1,8 +1,12 @@
 import axios from "axios";
 import { type Outfit } from "../assets/data";
 
+const API_URL =
+  import.meta.env.VITE_API_URL ||
+  "https://norisstitches-production.up.railway.app/products";
+
 const getProducts = async () => {
-  const response = await axios.get("http://localhost:5000/products");
+  const response = await axios.get(`${API_URL}/products`);
   return response.data as Outfit[];
 };
 export { getProducts };
