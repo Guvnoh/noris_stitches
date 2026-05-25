c4ca9814b011b10b3ddc344cb23ec9ac2ede2808
Guvnoh <asogwachukwuka@gmail.com>
2026-05-25 21:32:37 +0100

    remove unnecessary files and folders

--- src/Header.tsx
+++
@@ -1,8 +0,0 @@
-import "./header.css";
-export default function Header() {
-  return (
-    <header className="header bg-gray-800 text-white p-4">
-      <h1 className="text-2xl font-bold">Nori's Stitches</h1>
-    </header>
-  );
-}
